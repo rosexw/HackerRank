10844628ea3ec5c283805ab5d0e85adf711fd279
rosexw <rose.xw.wan@gmail.com>
2019-12-10 15:52:05 -0500

    Adding first few completed exercises from HackerRank to GitHub

---
+++ sets2.js
@@ -0,0 +1,13 @@
+// https://whatis.techtarget.com/definition/intersection-symbol
+
+
+// You are given two sets.
+// Set A = {1,2,3,4,5,6}
+// Set B = {2,3,4,5,6,7,8}
+
+// How many elements are present in A n B?
+// Only enter the correct integer in the answering box. Do not include any extra spaces, tabs or newlines.
+
+// Answer: 5
+
+// intersection = Given two sets A and B, the intersection of A and B, written A  B, is the set C of all elements that are in both A and B.
